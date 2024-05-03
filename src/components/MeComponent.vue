9c65af683a785df1fb39afbf243a2ecf9cdac8c9
RDDcat <yukmaro@gmail.com>
2024-05-04 01:01:52 +0900

    [UPDATE] ME 컴포넌트 UI 업데이트

--- src/components/MeComponent.vue
+++ src/components/MeComponent.vue
@@ -1,7 +1,38 @@
 <template>
+  <!--  -->
   <div class="w-full">
-    <spline-viewer url="https://prod.spline.design/E8hknpNGgGfjRLTv/scene.splinecode"></spline-viewer>
+    ㄴ
   </div>
+  <!-- 모바일 화면 -->
+  <div class="flex w-full">
+    <div class="flex m-auto w-4/5">
+      <spline-viewer class="mr-60 h-2/5" url="https://prod.spline.design/0-QULqF3MB8p6KT2/scene.splinecode"></spline-viewer>   
+    </div>
+  </div>
+  <div class="flex w-full">
+    <div class="absolute ml-60 mt-44 z-50">
+      <div class="text-7xl inline-block bg-gradient-to-r from-blue-700  to-indigo-400 text-transparent bg-clip-text">아이원잇</div>
+      <div class="flex text-base text-white">
+        희귀동물 입양/분양 커뮤니티 <div class="underline decoration-indigo-500">젊은</div>
+
+      </div>
+    </div>
+    <spline-viewer class="m-auto h-5/6" url="https://prod.spline.design/E8hknpNGgGfjRLTv/scene.splinecode"></spline-viewer>
+  </div>
+
+  <!--  -->
+
+  <!-- 텍스트 -->
+  <div class="text-7xl inline-block bg-gradient-to-r from-blue-700  to-indigo-400 text-transparent bg-clip-text">Build Fast</div>
+  <div class="flex text-base text-white">
+    빠르고 <div class="underline decoration-indigo-500">젊은</div>
+
+  </div>
+
+
+  <div class="h-96"></div>
+  <div class="h-96"></div>
+  <div class="h-96"></div>
 </template>
 
 <script>
