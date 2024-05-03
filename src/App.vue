82024e5b3d37d34100f6d103a7793eb147ebe461
RDDcat <yukmaro@gmail.com>
2024-05-03 19:12:41 +0900

    [UPDATE] tailwind css 추가 셋팅

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
 <template>
+<div class="w-full m-16">
+  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+
+</div>
+
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -18,9 +23,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 body{
   background-color: #04020C;
